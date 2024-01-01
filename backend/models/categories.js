b8ef20ec78771df9d8f8544c53d1a349438e3d1f
jihed0405 <jihed0405@gmail.com>
2024-01-02 00:46:35 +0100

    final code for the syndic app

---
+++ backend/models/categories.js
@@ -0,0 +1,15 @@
+module.exports = (sequelize,DataTypes)=>{
+    const Categorie =sequelize.define("Categorie",{
+        
+        NomCat:{
+        type:DataTypes.STRING,
+        primaryKey: true,
+        allowNull: false
+    },description:{
+        type:DataTypes.STRING,
+        allowNull: false
+    }
+}
+    );
+    return {name:'Categorie',schema: Categorie}  
+    }
